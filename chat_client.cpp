a0953f8a4e81d737abfb1022e88a2cf9479874b5
getsanjeev <getsanjeevdubey@gmail.com>
2017-09-26 02:06:24 +0530

    improved chat server

---
+++ chat_client.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+	int domain = AF_INET;
+	int type = SOCK_STREAM;
+	int protocol = 0;
+	int status = 0;
+	int file_descriptor = 0;
+	int port_number = 5010;	
+	struct sockaddr_in server_socket;   
+	char buffer[1024];
+
+	file_descriptor = socket(domain,type,protocol);
+	if(file_descriptor < 0){
+		cout<<"Error, failed to create sockets"<<endl;
+	}
+	else{		
+   			bzero((char *) &server_socket, sizeof(server_socket));
+   			server_socket.sin_family = AF_INET;   			
+   			server_socket.sin_addr.s_addr = inet_addr("127.0.0.1");
+   			server_socket.sin_port = htons(port_number);   			
+   			status = connect(file_descriptor, (struct sockaddr*)&server_socket, sizeof(server_socket));
+   			if(status < 0){
+   				cout<<"Could not connect, please try again..."<<endl; 
+   			}
+   			else{
+   				cout<<"status "<<status<<" ..........connected succesfully.........."<<endl;
+   				while(1){   	
+   					bzero(buffer,1024);				   								
+    				cin.getline( buffer, '\n');			    				
+					int n = write(file_descriptor,buffer,20);
+					if(n<0){
+						cout<<"ERROR reading from socket"<<endl;						
+					}
+					bzero(buffer,256);
+					n = read(file_descriptor,buffer,255);
+					cout<<buffer<<endl;					
+					if(n<0){
+						cout<<"Error writing to port"<<endl;
+					}
+   				}
+   				
+   			}
+	}
+}
---
+++ chat_server.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+using namespace std;
+
+/*struct in_addr {
+   unsigned long s_addr;
+};
+*/
+/*struct sockaddr_in {
+   short int            sin_family;
+   unsigned short int   sin_port;
+   struct in_addr       sin_addr;
+   unsigned char        sin_zero[8];
+};*/
+
+int main(){
+
+	int domain = AF_INET;
+	int type = SOCK_STREAM;
+	int protocol = 0;
+	int status = 0;
+	int file_descriptor = 0;
+	int port_number = 5010;
+	struct sockaddr_in server_socket, client_socket;	
+	socklen_t length_client;
+	char buffer[1024];
+
+	bzero((char *) &server_socket, sizeof(server_socket));
+	bzero((char *) &client_socket, sizeof(client_socket));
+
+	//initialising socket stucture
+	server_socket.sin_family = AF_INET;
+	server_socket.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server_socket.sin_port = htons(port_number);
+	length_client = sizeof(client_socket);
+
+	cout<<"Welcome"<<server_socket.sin_addr.s_addr<<" "<<server_socket.sin_port<<endl;
+	file_descriptor = socket(domain,type,protocol);
+	if(file_descriptor < 0){
+		cout<<"Error, failed to create sockets"<<endl;
+	}
+	else{
+		status =  bind(file_descriptor, (struct sockaddr *)&server_socket, sizeof(server_socket));
+		if(status < 0){
+			cout<<"Error, failed to bind to address"<<endl;
+		}
+		else{
+			status = listen(file_descriptor, 10);
+			if(status != 0){
+				cout<<"Error, failed to get to listen mode"<<endl;
+			}
+			else{
+				cout<<"Server has started....Please make requests to connect."<<endl;												
+
+				file_descriptor =  accept(file_descriptor, (struct sockaddr *)&client_socket, &length_client);
+				if(file_descriptor<0){
+					cout<<"Error, failed to create dedicated sockets for client"<<endl;
+				}
+				else{																
+					cout<<"HEY! CONNECTED SUCCESSFULLY"<<endl;
+					while(1){															
+						bzero(buffer,256);
+						int n = read(file_descriptor,buffer,255);
+						cout<<buffer<<endl;
+						if(n<0){
+							cout<<"ERROR reading from socket"<<endl;						
+						}
+						bzero(buffer,256);		
+    					cin.getline(buffer, '\n');
+						n = write(file_descriptor,buffer,12);
+						if(n<0){
+							cout<<"Error writing to port"<<endl;
+						}
+
+					}
+
+				}				
+			}
+		}
+	}
+}
